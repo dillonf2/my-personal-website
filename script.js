19d129347cddd94b8a36dc1959f1439ff59531c5
Frank Dillon <dillonf2@outlook.com>
2023-02-15 17:23:36 -0500

    added full button functionality

--- script.js
+++ script.js
@@ -2,16 +2,29 @@
 const p=document.createElement("p");
 p.textContent="Created via JavaScript!"
 
-function addingEventListener(){
-    function clickAlert(){
-        alert(`Great Job! You're good at listening to instructions!`)
-    }
-    theButton.addEventListener('click', clickAlert)
-}
 const theButton=document.getElementById("button")
-addingEventListener(theButton)
+let theButtonTracker=`unclicked`
 
 theButton.addEventListener('click',function(){
     theButton.style.color='purple'
     theButton.style.textDecoration='underline'
 })
+
+addingEventListener()
+function addingEventListener(){
+	
+	// this entire function is called on every click
+	function clickAlert(){
+		if (theButtonTracker==='unclicked'){
+			alert(`Great Job! You're good at listening to instructions!`)
+			theButton.innerHTML="Don't click me again!"
+			theButtonTracker=`clicked`
+		} else {
+			alert(`You Fool! Listen to the instructions!`)
+			theButton.innerHTML="Click Me!"
+			theButtonTracker=`unclicked`
+		}
+	}
+	
+	theButton.addEventListener('click', clickAlert)
+}
